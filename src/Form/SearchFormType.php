aa7179b2742542f9e59b7a09dc2b44eefd8b9e51
Lucas-Feat <lucas.feat.lf@gmail.com>
2022-03-09 13:44:01 +0100

    Init Form&Class for Search

--- src/Form/SearchFormType.php
+++ src/Form/SearchFormType.php
@@ -2,8 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Form\Model\SearchOuting;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +15,15 @@ class SearchFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name');
+            ->add('campus', Campus::class)
+            ->add('search', TextType::class)
+            ->add('dateStarted', \DateTime::class)
+            ->add('dateEnded', \DateTime::class)
+            ->add('isOrganized', CheckboxType::class)
+            ->add('isRegistered', CheckboxType::class)
+            ->add('isNotRegistered', CheckboxType::class)
+            ->add('isOver', CheckboxType::class);
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
