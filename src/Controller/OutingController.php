91aff2af13e5742d9548ebd2bda2f58fdb998bdc
Lucas-Feat <lucas.feat.lf@gmail.com>
2022-03-09 10:57:15 +0100

    Recherche par campus OK

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\Outing;
 use App\Entity\Campus;
+use App\Entity\Outing;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +15,20 @@ class OutingController extends AbstractController
     /**
      * @Route("/outing", name="app_outing")
      */
-    public function index(): Response
+    public function index(Request $req, EntityManagerInterface $mgr): Response
     {
-        $outings = $this->getDoctrine()->getManager()->getRepository(Outing::class)->findAll();
-        $campus = $this->getDoctrine()->getManager()->getRepository(Campus::class)->findAll();
+
+        if ('submit' === $req->query->get('submit')) {
+            $searchCampus = $mgr->getRepository(Campus::class)->find($req->query->get('campus'));
+            
+            $outings = $mgr->getRepository(Outing::class)->findBy(array('campus' => $searchCampus));
+        } else {
+            $outings = $mgr->getRepository(Outing::class)->findAll();
+            //TODO : à mettre en place une fois les log en place
+            //$outings = $mgr->getRepository(Outing::class)->findBy(array('campus' => $this->getUser()->getCampus()));
+        }
+        $campus = $mgr->getRepository(Campus::class)->findAll();
+
         return $this->render('outing/index.html.twig', [
             'controller_name' => 'OutingController',
             'outings' => $outings,
