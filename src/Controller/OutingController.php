2b75271fccc53f1dcbf99d65fd0289d467cf36ac
Lucas-Feat <lucas.feat.lf@gmail.com>
2022-03-09 11:52:25 +0100

    Recherche par date OK

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -18,10 +18,25 @@ class OutingController extends AbstractController
     public function index(Request $req, EntityManagerInterface $mgr): Response
     {
 
-        if ('submit' === $req->query->get('submit')) {
-            $searchCampus = $mgr->getRepository(Campus::class)->find($req->query->get('campus'));
-            
+        if ($req->request->get('submit')) {
+            $searchCampus = $mgr->getRepository(Campus::class)->find($req->request->get('campus'));
             $outings = $mgr->getRepository(Outing::class)->findBy(array('campus' => $searchCampus));
+            if ($req->request->get('dateStarted')) {
+                $dateStarted = date_create_from_format('Y-m-d', $req->request->get('dateStarted'));
+                $outings = array_filter($outings, (function ($o) use ($dateStarted) {
+                    if ($o->getStartDate() > $dateStarted) {
+                        return $o;
+                    }
+                }));
+            }
+            if ($req->request->get('dateEnded')) {
+                $dateEnded = date_create_from_format('Y-m-d', $req->request->get('dateEnded'));
+                $outings = array_filter($outings, (function ($o) use ($dateEnded) {
+                    if ($o->getStartDate() < $dateEnded) {
+                        return $o;
+                    }
+                }));
+            }
         } else {
             $outings = $mgr->getRepository(Outing::class)->findAll();
             //TODO : à mettre en place une fois les log en place
