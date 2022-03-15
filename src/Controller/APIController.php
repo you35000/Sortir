97b7c9ffd93ea81d493a06ba405693d0d62ae7b5
mjubault2021 <mathieu.jubault2021@campus-eni.fr>
2022-03-15 12:31:48 +0100

    message

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Place;
 use App\Repository\CityRepository;
 use App\Repository\PlaceRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Email;
 
 /**
  * @Route("/api")
@@ -37,4 +42,21 @@ class APIController extends AbstractController
     {
         return $this->render('api/index.html.twig');
     }
+    /**
+     * @Route("/places/", name="api_places_post", methods={"POST"})
+     */
+    public function update(Request $req,EntityManagerInterface $em): Response
+    {
+        $place = new Place();
+        $data = json_decode($req->getContent());
+        $place->setCity($data->city);
+        $place->setLatitude($data->latitude);
+        $place->setLongitude($data->longitude);
+        $place->setName($data->name);
+        $place->setStreet($data->street);
+        $em->persist($place);
+        $em->flush();
+        return $this->json($place);
+    }
+
 }
